436dd365d69a1b6e38eaa3171808e10e754fb123
Ethney Stewart <91434172+ethneystewart@users.noreply.github.com>
2026-03-22 11:12:24 -0600

    move DB config to env variables

--- app/database/db.py
+++ app/database/db.py
@@ -1,9 +1,11 @@
+import os
 import psycopg2
 
 conn = psycopg2.connect(
-    host="localhost",
-    database="moodtracker",
-    user="ethneystewart"
+    host=os.getenv("DB_HOST", "db"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
 )
 
 cursor = conn.cursor()
